15a875728012ea9a36e2bd1e277c988bed4745f9
sebastian <sebu9495@gmail.com>
2024-04-23 19:38:08 +0400

    screenshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,35 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'canvascaputure';
+  @ViewChild('body') body!:ElementRef;
+public darkImage = '';
+public LightImage = '';
+constructor(private _render2: Renderer2,private _cdr: ChangeDetectorRef)
+{
+
+}
+
+  capture() {
+    // use opposite theme
+    this._render2.setStyle(this.body.nativeElement,'background' ,'#9c9ca0' );
+    this.canvasRender(this.body.nativeElement).then( (result) => {
+      this.LightImage = result;
+      this.canvasRender(this.body.nativeElement).then((resultNew) => {
+        this.darkImage = resultNew;
+        this._cdr.detectChanges();
+      })
+    }
+    )
+      // use current  theme
+  this._render2.removeStyle(this.body.nativeElement,'background' );
+
+  }
+  canvasRender(element: HTMLElement) {
+   return new Promise((resolve:(value: string) => void, reject) => {
+    html2canvas(element).then(canvas => {
+      resolve(canvas.toDataURL("image/png"));
+  });
+    });
+
+  }
 }
